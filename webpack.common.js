5d7e42b37cff26bfbc5d2ee55988d1b563359331
Giancarlo Soverini <giancarlosoverini@gmail.com>
2018-09-13 17:00:12 -0400

    fix: Re-add dist folder

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const {CheckerPlugin} = require('awesome-typescript-loader');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 // TODO minify CSS and JS?
 module.exports = merge(common, {
