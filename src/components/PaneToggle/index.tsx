8b755336d9d1f9b0f38be6f618bb1a82ff556d5f
Giancarlo Soverini <giancarlo.soverini@bynd.com>
2018-09-24 13:16:20 -0400

    chore: Update Package.json

--- src/components/PaneToggle/index.tsx
+++ src/components/PaneToggle/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { WithStyles, withStyles } from '@material-ui/core';
 import ButtonBase from '@material-ui/core/ButtonBase';
+import { VisiblePanesCount } from '../PaneGrid';
 import styles from './styles';
 
 export interface PaneToggleProps extends WithStyles<typeof styles> {
-  onToggleView: (nOfPanes: number) => void;
+  onToggleView: (nOfPanes: VisiblePanesCount) => void;
 }
 
 class PaneToggle extends React.Component<PaneToggleProps> {
