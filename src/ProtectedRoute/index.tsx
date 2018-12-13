c60d60fa16f954638715ed5e002749b79ea5df33
Giancarlo Soverini <giancarlosoverini@gmail.com>
2018-12-12 20:30:12 -0500

    feat: Add Auth reducer and actions

--- src/ProtectedRoute/index.tsx
+++ src/ProtectedRoute/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { URLS } from 'src/routes';
+import { URLS } from 'src/rootRoutes';
 
 const ProtectedRoute = ({ isAllowed, ...props }: any) => {
   return isAllowed ? <Route {...props}/> : <Redirect to={URLS.Login} />;
