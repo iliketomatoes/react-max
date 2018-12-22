29f08e06b3444a122704b43b47fbc45d42580f34
Giancarlo Soverini <giancarlosoverini@gmail.com>
2018-12-22 10:32:27 -0500

    refactor: Remove redundant Auth actions

--- src/Auth/actions.ts
+++ src/Auth/actions.ts
@@ -12,21 +12,15 @@ export interface LoginRequestPayload {
   password: string;
 }
 
-/** Action creator for the login request. The payload contains the username and password requested. */
+/** Async action creator for the login request. The payload contains the username and password requested. */
 export const loginRequest = actionCreator
     .async<LoginRequestPayload, AccessToken, AuthError>('LOGIN_REQUEST');
 
 /** Action creator for the logout request. */
 export const logoutRequest = actionCreator('LOGOUT_REQUEST');
 
-/** The user has been logged in. The payload is the OAuth access token. */
-export const loggedIn = actionCreator<AccessToken>('LOGGED_IN');
-
 /** The user has been logged out. */
 export const loggedOut = actionCreator('LOGGED_OUT');
-
-/** An error has occurred while trying to login. */
-export const loginError = actionCreator<AuthError>('LOGIN_ERROR');
 
 /** An error has occurred while the user is logged in, either logging out or refreshing the token. */
 export const loggedInError = actionCreator<AuthError>('LOGGED_IN_ERROR');
--- src/Auth/reducer.ts
+++ src/Auth/reducer.ts
@@ -19,21 +19,17 @@ const INITIAL_STATE: StoreState = {
 };
 
 export const reducer = reducerWithInitialState(INITIAL_STATE)
-    /* The user has been logged in: store the access token in the state. */
-    .case(actions.loggedIn, (state, accessToken) => {
-        return { ...state, accessToken };
-    })
     /* The user has been logged out; remove our stored access token from the state. */
-    .case(actions.loggedOut, (state) => {
+    .case(actions.loggedOut, (state: StoreState) => {
         return { ...state, accessToken: undefined };
-    })
-    .case(actions.loginError, (state, error) => {
-        return { ...state, error: error };
     })
     .case(actions.loginRequest.started, (state: StoreState) => {
         return { ...state, error: undefined };
     })
     .case(actions.loginRequest.done, (state: StoreState, payload) => {
       return { ...state, error: undefined, access_token: payload.result.access_token };
+    })
+    .case(actions.loginRequest.failed, (state: StoreState, payload) => {
+        return { ...state, error: payload.error };
     })
     ;
