9b92db3e341c11ddf031db36a09b8b678d8c5de8
Giancarlo Soverini <giancarlosoverini@gmail.com>
2019-02-03 22:03:43 -0500

    feat: Implement graphql-request library

--- src/Api/functions.ts
+++ src/Api/functions.ts
@@ -1,23 +1,16 @@
 import { Observable, Observer } from 'rxjs';
+import { request } from 'graphql-request';
 import { QueryPayload } from './types';
 import { apiEndpoint } from './config';
 
 export const makeQuery = <T extends any>(query: string): Observable<QueryPayload<T>> => {
   return Observable.create((observer: Observer<QueryPayload<T>>) => {
-    fetch(apiEndpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
-      body: JSON.stringify({query: query})
-    })
-    .then(response => response.json())
-    .then((res: {data: T}) => {
+    request(apiEndpoint, query)
+    .then((res: T) => {
       observer.next({
         started: false,
         failed: false,
-        data: res.data
+        data: res
       });
     })
     .catch((error) => {
